baaa8adf1d936d4571565e4f8dc78614dfa176a2
Michael Douglas <michael.douglas@atitude.com.br>
2017-09-10 15:22:22 -0300

    Ajuste da documentacao

--- lambda-config.js
+++ lambda-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    region: 'us-east-1',
-    handler: 'index.handler',
-    role: 'arn:aws:iam::525203983100:role/lambdaPowerUser',
-    functionName: 'some-function', //Remember: of change this here line. for the your function AWS lambda
+    region: 'REGION DO SEU LAMBDA',
+    handler: 'Handler DO SEU LAMBDA',
+    role: 'ARN DO SEU LAMBDA',
+    functionName: 'NOME DO SEU LAMBDA', //Remember: of change this here line. for the your function AWS lambda
     timeout: 60
 }
